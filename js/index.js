af72e2a30f6f6ba06f78e87882e34dac30a7dfed
baikeqi <baikeqii@163.com>
2019-01-21 00:43:41 +0800

    page done

--- js/index.js
+++ js/index.js
@@ -1,26 +1,75 @@
-$(function () {
-    layer.ready(function(){
-        layer.open({
-            type: 1,
-            title: false,
-            skin: 'layui-layer-demo',
-            closeBtn: false,
-            shadeClose: true,
-            area: '700px;',
-            shade: 0.3,
-            id: 'LAY_layuipro',
-            btnAlign: 'c',
-            moveType: 1,
-            content: $(".indexNameModal"),
-            success: function(layero){
+$(() =>
+{
+    getAutomaticLayer();
+    handleUploadFile();
+    handleLoginUser();
+    $(".videoNavItem").on("mouseover", toggleVideoAds);
+});
+
+function getAutomaticLayer()
+{
+    layer.ready(() =>
+    {
+        getSignUpNameModal();
+
+        let fiveMinInterval = setInterval(() =>
+        {
+            getFiveMinModal();
+            clearInterval(fiveMinInterval)
+        }, 300000)
+    });
+}
+
+function handleUploadFile() {
+    layui.use('upload', function(){
+        let upload = layui.upload,
+            uploadInst = upload.render({
+            elem: '#uploadPic'
+            ,url: '/upload/' //上传接口
+            ,done: function(res){
+
+            }
+            ,error: function(){
 
             }
         });
+        //uploadInst.upload(); 可重新上传 可全局
     });
+}
 
-    layui.use(['form'], function() {
-        var form = layui.form;
-        form.on('submit(loginForm)', function(data)
+function getFiveMinModal()
+{
+    layer.open({
+        type: 1,
+        title: false,
+        skin: 'layui-layer-demo',
+        shadeClose: true,
+        area: '700px;',
+        id: 'LAY_fiveMin',
+        btnAlign: 'c',
+        moveType: 1,
+        content: $(".fiveMinutesWrapper"),
+        cancel: function(index, layero){
+            layer.close(index)
+        }
+    });
+}
+
+function toggleVideoAds()
+{
+    let imgShowItem = $(this).parents(".videoBanner").children(".videoNavImg").eq($(this).index());
+    $(this).addClass("hoverItem");
+    $(this).siblings().removeClass("hoverItem");
+    imgShowItem.addClass("imgShowItem");
+    imgShowItem.siblings().removeClass("imgShowItem");
+}
+
+function handleLoginUser()
+{
+    layui.use(['form'],() =>
+    {
+        let form = layui.form;
+        form.on('submit(loginForm)', data =>
         {
             layer.alert(JSON.stringify(data.field), {
                 title: '最终的提交信息'
@@ -28,43 +77,53 @@ $(function () {
             return false;
         });
     });
-    $(".videoNavItem").on("mouseover", function () {
-        var imgShowItem = $(this).parents(".videoBanner").children(".videoNavImg").eq($(this).index());
-        $(this).addClass("hoverItem");
-        $(this).siblings().removeClass("hoverItem");
-        imgShowItem.addClass("imgShowItem");
-        imgShowItem.siblings().removeClass("imgShowItem");
-    });
-});
+}
 
-window.onload(function () {
-
-})
+function getSignUpNameModal()
+{
+    layer.ready(() =>
+    {
+        layer.open({
+            type: 1,
+            title: false,
+            skin: 'layui-layer-demo',
+            shadeClose: true,
+            area: '700px;',
+            shade: 0.3,
+            id: 'LAY_signUp',
+            btnAlign: 'c',
+            moveType: 1,
+            content: $(".indexNameModal"),
+            cancel: function(index, layero){
+                layer.close(index)
+            }
+        });
+    })
+}
 
 function handleChangeServiceTab(ele)
 {
     $(ele).parents(".asideRightTab").toggleClass("asideShow");
     $(ele).parents(".asideRightTab").siblings(".asideRightTab").toggleClass("asideShow");
 }
+
 function handleShowLoginModal()
 {
     layer.open({
         type: 1,
-        title: false, //不显示标题栏
+        title: false,
         skin: 'layui-layer-demo',
         closeBtn: false,
         shadeClose: true,
         area: '540px;',
         shade: 0.8,
-        id: 'LAY_layuipro', //设定一个id，防止重复弹出
+        id: 'LAY_login',
         btnAlign: 'c',
         moveType: 1, //拖拽模式，0或者1
-        content: $(".loginWrapper"),
-        success: function(layero){
-
-        }
+        content: $(".loginWrapper")
     });
 }
+
 function handleShowRegisterModal()
 {
     layer.open({
@@ -75,16 +134,24 @@ function handleShowRegisterModal()
         shadeClose: true,
         area: '600px;',
         shade: 0.8,
-        id: 'LAY_layuipro',
+        id: 'LAY_register',
         btnAlign: 'c',
         moveType: 1,
-        content: $(".registerWrapper"),
-        success: function(layero){
-
-        }
+        content: $(".registerWrapper")
     });
 }
 
-function handleCloseRegisterModal() {
-    layer.closeAll()
+function handleCloseModal()
+{
+    layer.close(layer.index);
+}
+
+function handleScroll(ele)
+{
+    let currentEle = $(ele).children(".recordIcon"),
+        currentCls = currentEle.attr("class"),
+        isChecked = currentCls.includes("checkedStatus"),
+        toggleCls = isChecked ? "recordIcon unCheckedStatus" : "recordIcon checkedStatus";
+
+        currentEle.attr("class", toggleCls)
 }
